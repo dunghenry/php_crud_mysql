a316a91382b894c7e41e518f3941ba819ae819b2
dunghenry <trandungksnb6930082001>
2022-11-30 19:45:53 +0700

    add modal and run local

--- display.php
+++ display.php
@@ -1,59 +1,82 @@
-<?php include 'connect.php';?>
+<?php include 'connect.php'; ?>
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>PHP_MYSQL</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  </head>
-  <body>
-    <div class="container my-5">
-        <button class="btn btn-primary">
-            <a href="user.php" class="text-light text-decoration-none">Add User</a>
-        </button>
-        <table class="table">
-            <thead>
-                <tr>
-                <th scope="col">Sl no</th>
-                <th scope="col">Name</th>
-                <th scope="col">Email</th>
-                <th scope="col">Mobile</th>
-                <th scope="col">Password</th>
-                <th scope="col">Operations</th>
-                </tr>
-            </thead>
-            <tbody>
-               <?php
-               $sql = "SELECT * FROM users";
-               $result = mysqli_query($conn, $sql);
-               if (mysqli_num_rows($result) > 0) {
-                while($row = mysqli_fetch_assoc($result)) {
-                  $id = $row['id'];
-                  $name = $row['name'];
-                  $email = $row['email'];
-                  $mobile = $row['mobile'];
-                  $password = $row['password'];
-                echo '<tr>
-                <th scope="row">'.$id.'</th>
-                <td>'.$name.'</td>
-                <td>'.$email.'</td>
-                <td>'.$mobile.'</td>
-                <td>'.$password.'</td>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>PHP_MYSQL</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+
+<body>
+  <div class="container my-5">
+    <button class="btn btn-primary">
+      <a href="user.php" class="text-light text-decoration-none">Add User</a>
+    </button>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Sl no</th>
+          <th scope="col">Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">Mobile</th>
+          <th scope="col">Password</th>
+          <th scope="col">Operations</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        $sql = "SELECT * FROM users";
+        $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) > 0) {
+          while ($row = mysqli_fetch_assoc($result)) {
+            $id = $row['id'];
+            $name = $row['name'];
+            $email = $row['email'];
+            $mobile = $row['mobile'];
+            $password = $row['password'];
+            echo '<tr>
+                <th scope="row">' . $id . '</th>
+                <td>' . $name . '</td>
+                <td>' . $email . '</td>
+                <td>' . $mobile . '</td>
+                <td>' . $password . '</td>
                 <td>
-                    <button class="btn btn-primary"><a href="update.php?id='.$id.'" class="text-light text-decoration-none">Edit</a></button>&nbsp;&nbsp;&nbsp;
-                    <button class="btn btn-danger"><a href="delete.php?id='.$id.'" class="text-light text-decoration-none">Delete</a></button>
+                     <button class="btn btn-primary"><a href="update.php?id=' . $id . '" class="text-light text-decoration-none">Edit</a></button>&nbsp;&nbsp;&nbsp;
+                    <button type="button" class="btn btn-warning text-light" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        Delete
+                    </button>
+                    <!-- Modal -->
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Notification</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            Confirm delete item
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancle</button>
+                            <button type="button" class="btn btn-danger"><a href="delete.php?id=' . $id . '" class="text-light text-decoration-none">Confirm</a></button>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
                 </td>
                </tr>';
-                }
-              } else {
-                echo "0 results";
-              }
-                    
-               ?>
-            </tbody>
-        </table>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-  </body>
+          }
+        } else {
+          echo "0 results";
+        }
+
+        ?>
+      </tbody>
+    </table>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</body>
+
 </html>
